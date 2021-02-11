d6ac7c0dd445ea33b6459958ab15b01ccc8813ba
agonmustafa1 <58729229+agonmustafa1@users.noreply.github.com>
2021-02-11 21:17:14 +0100

    Update serverGUI.py

---
+++ serverGUI.py
@@ -0,0 +1,19 @@
+#pjesa e serverit
+#importimi i librarive te nevojshme
+from cryptography.fernet import Fernet
+from tkinter import *
+from tkinter import font 
+from tkinter import ttk 
+import socket 
+import threading 
+
+
+
+PORT = 13000 
+SERVER = "localhost" 
+ADDRESS = (SERVER, PORT) 
+FORMAT = "utf-8"
+  
+#Krijimi i nje socket-i per konektim
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+client.connect(ADDRESS)
