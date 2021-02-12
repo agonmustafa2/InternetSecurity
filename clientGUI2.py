d4c2502d1d511cb0ca79dc82e9d63bb0fec2bb58
JetmirK <59027108+JetmirK@users.noreply.github.com>
2021-02-12 20:14:13 +0100

    Update clientGUI2.py

--- clientGUI2.py
+++ clientGUI2.py
@@ -18,3 +18,38 @@ server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #e bind adresen  e seeverit ne soket 
 server.bind(ADDRESS) 
   
+#fillimi i konektimit
+def startChat(): 
+    
+    print("Serveri " + SERVER) 
+      
+    #shikon te gjitha konektimet 
+    server.listen() 
+      
+    while True: 
+        
+        #prano konektimet dhe kthen konektime te reja te klienti dhe adreses se lidhur me te
+        conn, addr =  server.accept() 
+        conn.send("NAME".encode(FORMAT)) 
+          
+        #tregon sasin e te dhenave qe mund te pranohet
+        name = conn.recv(2048).decode(FORMAT) 
+          
+       #tregon emrin e klientit
+        names.append(name) 
+        clients.append(conn) 
+          
+        print(f"Emri perdoruesit :{name}") 
+          
+        #mesazhi i transmetuar
+        broadcastMessage(f"{name} has joined the chat".encode(FORMAT)) 
+          
+        conn.send(''.encode(FORMAT)) 
+          
+        #fillom thread handling
+        thread = threading.Thread(target = handle, 
+                                  args = (conn, addr)) 
+        thread.start() 
+          
+        #numri i klientave te konektuar
+        print(f"Numri koneksioneve aktive {threading.activeCount()-1}")
